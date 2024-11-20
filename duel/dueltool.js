03650d26880e7eeb6e11771984abf0ca626e9f11
Straevaras <gary.weasel@gmail.com>
2024-11-19 19:15:12 -0500

    Add files via upload

---
+++ duel/dueltool.js
@@ -0,0 +1,563 @@
+var ver_major = 0
+var ver_minor = 3
+var ver_revision = 1
+var p1Sel, p2Sel, iBan, iReq, pBan, pReq, boss, maxM, maxS, maxP, maxE, maxR;
+var pids = ['#p1c1', '#p2c1', '#p2c2', '#p1c2'];
+var lib = {'charge': 'Charge Beam',
+		   'ice': 'Ice Beam',
+		   'wave': 'Wave Beam', 
+		   'spazer': 'Spazer Beam',
+		   'plasma': 'Plasma Beam',
+		   'varia': 'Varia Suit',
+		   'gravity': 'Gravity Suit',
+		   'screw': 'Screw Attack',
+		   'hijump': 'Hi-Jump Boots',
+		   'speed': 'Speed Booster',
+		   'sj': 'Space Jump',
+		   'springball': 'Springball',
+		   'grapple': 'Grapple Beam',
+		   'xray': 'X-Ray Scope',
+		   'missiles': 'Missiles',
+		   'supers': 'Super Missiles',
+		   'pbs': 'Power Bombs',
+		   'ets': 'Energy Tanks',
+		   'reserve': 'Reserve Tanks',
+		   'kraid': 'Kraid Last',
+		   'phantoon': 'Phantoon Last',
+		   'draygon': 'Draygon Last',
+		   'ridley': 'Ridley Last',
+		   'sporespawn': 'Spore Spawn First',
+		   'crocomire': 'Crocomire First',
+		   'botwoon': 'Botwoon First'};
+
+function initTool() {
+	$('select').prop('disabled', true).empty();
+	$('button:contains("Apply")').prop('disabled', true);
+	$('#goal').html("");
+	
+	p1Sel = p2Sel = ['Ban', 'Require'];
+	iBan = iReq = [lib['charge'],
+				   lib['ice'],
+				   lib['wave'],
+				   lib['spazer'],
+				   lib['plasma'],
+				   lib['varia'],
+				   lib['gravity'],
+				   lib['screw'],
+				   lib['hijump'],
+				   lib['speed'],
+				   lib['sj'],
+				   lib['springball'],
+				   lib['grapple'],
+				   lib['xray']];
+	pBan = pReq = [lib['missiles'],
+				   lib['supers'],
+				   lib['pbs'],
+				   lib['ets'],
+				   lib['reserve']];
+	boss = [lib['kraid'],
+			lib['phantoon'],
+			lib['draygon'],
+			lib['ridley']];
+	mboss = [lib['sporespawn'],
+			 lib['crocomire'],
+			 lib['botwoon']];
+
+	maxM = 230;
+	maxS = 50;
+	maxP = 50;
+	maxE = 14;
+	maxR = 4;
+
+	applyChoice(0);
+}
+
+function applyBan(sel) {
+	iBan = remove(iBan, sel);
+	iReq = remove(iReq, sel);
+	pBan = remove(pBan, sel);
+	pReq = remove(pReq, sel);
+	switch(sel) {
+		case lib['charge']:
+			pBan = remove(pBan, lib['missiles']);
+			break;
+		case lib['ice']:
+			iBan = remove(iBan, lib['gravity']);
+			boss = remove(boss, lib['phantoon']);
+			mboss = remove(mboss, lib['botwoon']);
+			iBan = remove(iBan, lib['speed']);
+			if (isBanned(lib['grapple'])) {
+				iReq = remove(iReq, lib['springball'])
+			}
+			break;
+		case lib['wave']:
+			maxE -= 1;
+			break;
+		case lib['varia']:
+			if (isBanned(lib['gravity']) || isRequired(lib['phantoon']))  {
+				pBan = remove(pBan, lib['ets']);
+				pBan = remove(pBan, lib['reserve']);
+			}
+			break;
+		case lib['gravity']:
+			iBan = remove(iBan, lib['ice']);
+			iBan = remove(iBan, lib['hijump']);
+			iBan = remove(iBan, lib['grapple']);
+			iBan = remove(iBan, lib['xray']);
+			iReq = remove(iReq, lib['springball']);
+			pBan = remove(pBan, lib['missiles']);
+			pBan = remove(pBan, lib['supers']);
+			pBan = remove(pBan, lib['pbs']);
+			maxP -= 5;
+			maxR -= 1;
+			if (!isBanned(lib['speed'])) {
+				maxM -= 10;
+				maxS -= 5;
+				if (isPRequired(lib['reserve'], 3)) {
+					iBan = remove(iBan, lib['speed']);
+				}
+			}
+			if (isBanned(lib['varia']) || isRequired(lib['kraid']))  {
+				pBan = remove(pBan, lib['ets']);
+				pBan = remove(pBan, lib['reserve']);
+			}
+			break;
+		case lib['hijump']:
+			iBan = remove(iBan, lib['gravity']);
+			boss = remove(boss, lib['phantoon']);
+			mboss = remove(mboss, lib['botwoon']);
+			break;
+		case lib['speed']:
+			iBan = remove(iBan, lib['ice']);
+			maxR -= 1;
+			maxE -= 1;
+			maxS -= 5;
+			if (!isBanned(lib['gravity'])) {
+				maxM -= 10;
+				maxS -= 5;
+				if (isPRequired(lib['reserve'], 3)) {
+					iBan = remove(iBan, lib['gravity']);
+				}
+			}
+			break;
+		case lib['sj']:
+			maxM -= 5;
+			break;
+		case lib['grapple']:
+			mboss = remove(mboss, lib['botwoon']);
+			if (isBanned(lib['ice'])) {
+				iReq = remove(iReq, lib['springball'])
+			}
+			break;
+		case lib['xray']:
+			iBan = remove(iBan, lib['gravity']);
+			boss = remove(boss, lib['phantoon']);
+			break;
+		case lib['missiles']:
+			iBan = remove(iBan, lib['charge']);
+			iBan = remove(iBan, lib['gravity']);
+			pBan = remove(pBan, lib['supers']);
+			boss = remove(boss, lib['phantoon']);
+			break;
+		case lib['supers']:
+			iBan = remove(iBan, lib['gravity']);
+			pBan = remove(pBan, lib['missiles']);
+			boss = remove(boss, lib['phantoon']);
+			break;
+		case lib['pbs']:
+			iBan = remove(iBan, lib['gravity']);
+			boss = remove(boss, lib['phantoon']);
+			break;
+		case lib['ets']:
+			if (isBanned(lib['varia']) || isRequired(lib['kraid'])) {
+				iBan = remove(iBan, lib['gravity']);
+				boss = remove(boss, lib['phantoon'])
+			}
+			if (isBanned(lib['gravity'], iBan) || isRequired(lib['phantoon'])) {
+				iBan = remove(iBan, lib['varia']);
+				boss = remove(boss, lib['kraid']);
+			}
+			break;
+		case lib['reserve']:
+			if (isBanned(lib['varia']) || isRequired(lib['kraid'])) {
+				iBan = remove(iBan, lib['gravity']);
+				boss = remove(boss, lib['phantoon'])
+			}
+			if (isBanned(lib['gravity'], iBan) || isRequired(lib['phantoon'])) {
+				iBan = remove(iBan, lib['varia']);
+				boss = remove(boss, lib['kraid']);
+			}
+			break;
+	}
+}
+
+function applyReq(sel) {
+	iBan = remove(iBan, sel);
+	iReq = remove(iReq, sel);
+	pBan = remove(pBan, sel);
+	pReq = remove(pReq, sel);
+	boss = remove(boss, sel);
+	mboss = remove(mboss, sel);
+	switch(sel) {
+		case lib['springball']:
+			iBan = remove(iBan, lib['gravity']);
+			if (isBanned(lib['ice'])) {
+				iBan = remove(iBan, lib['grapple'])
+				if (ifBanned(lib['hijump'])) {
+					iBan = remove(iBan, lib['sj'])
+				}
+				else if (isBanned(lib['sj'])) {
+					iBan = remove(iBan, lib['hijump'])
+				}
+			}
+			else if (isBanned(lib['grapple'])) {
+				iBan = remove(iBan, lib['ice'])
+			}
+			
+			break;
+		case lib['kraid']:
+			if (isBanned(lib['gravity']))  {
+				pBan = remove(pBan, lib['missiles']);
+				pBan = remove(pBan, lib['supers']);
+				pBan = remove(pBan, lib['pbs']);
+				pBan = remove(pBan, lib['ets']);
+				pBan = remove(pBan, lib['reserve']);
+			}
+			if (isBanned(lib['missiles']) || isBanned(lib['supers']) || isBanned(lib['pbs']) ||
+				isBanned(lib['ets']) || isBanned(lib['reserve'])) {
+				iBan = remove(iBan, lib['gravity']);
+			}
+			break;
+		case lib['phantoon']:
+			iBan = remove(iBan, lib['ice']);
+			iBan = remove(iBan, lib['hijump']);
+			iBan = remove(iBan, lib['grapple']);
+			iBan = remove(iBan, lib['xray']);
+			pBan = remove(pBan, lib['missiles']);
+			pBan = remove(pBan, lib['supers']);
+			pBan = remove(pBan, lib['pbs']);
+			if (isBanned(lib['varia']))  {
+				pBan = remove(pBan, lib['ets']);
+				pBan = remove(pBan, lib['reserve']);
+			}
+			if (isBanned(lib['missiles']) || isBanned(lib['supers']) || isBanned(lib['pbs']) ||
+				isBanned(lib['ets']) || isBanned(lib['reserve'])) {
+				iBan = remove(iBan, lib['varia']);
+			}
+			break;
+		case lib['botwoon']:
+			iBan = remove(iBan, lib['grapple']);
+			iBan = remove(iBan, lib['ice']);
+			iBan = remove(iBan, lib['hi-jump']);
+			break;
+	}
+}
+
+function applyPReq(sel, num) {	 
+	pBan = remove(pBan, sel);
+	pReq = remove(pReq, sel);
+	switch(sel) {
+		case lib['missiles']:
+			if (num >= 220) {
+				iBan = remove(iBan, lib['gravity']);
+				iBan = remove(iBan, lib['speed']);
+				iBan = remove(iBan, lib['sj']);
+			}
+			break;
+		case lib['supers']:
+			if (num >= 45) {
+				iBan = remove(iBan, lib['speed']);
+			}
+			if (num == 50) {
+				iBan = remove(iBan, lib['gravity']);
+			}
+			break;
+		case lib['pbs']:
+			if (num == 50) {
+				iBan = remove(iBan, lib['gravity']);
+			}
+			break;
+		case lib['ets']:
+			if (num >= 13) {
+				iBan = remove(iBan, lib['speed']);
+				iBan = remove(iBan, lib['wave']);
+			}
+			break;
+		case lib['reserve']:
+			if (num == 4) {
+				iBan = remove(iBan, lib['speed']);
+				iBan = remove(iBan, lib['gravity']);
+			}
+			if (num == 3 && (isBanned(lib['gravity']) || isBanned(lib['speed']))) {
+				if (isBanned(lib['gravity'])) {
+					iBan = remove(iBan, lib['speed']);
+				}
+				else if (isBanned(lib['speed'])) {
+					iBan = remove(iBan, lib['gravity']);
+				}
+			}
+			break;
+	}
+}
+
+function isBanned(value) {
+	for (var i = 0; i < pids.length; i++) {
+		if ($(pids[i] + "_choice option:selected").text() == "Ban") 
+			if ($(pids[i] + "_sel option:selected").text() == value) 
+				return true;
+	}
+	return false;
+}
+
+function isRequired(value) {
+	for (var i = 0; i < pids.length; i++) {
+		if ($(pids[i] + "_choice option:selected").text() == "Require") 
+			if ($(pids[i] + "_sel option:selected").text() == value) 
+				return true;
+	}
+	return false;
+}
+
+function isPRequired(value, num) {
+	for (var i = 0; i < pids.length; i++) {
+		if ($(pids[i] + "_choice option:selected").text() == "Require") 
+			if ($(pids[i] + "_sel option:selected").text() == value)
+				if ($(pids[i] + "_num option:selected").text() >= num)
+					return true;
+	}
+	return false;
+}
+
+function remove(array, item) {
+	array = jQuery.grep(array, function(value) {
+		return value != item;
+	});
+	return array;
+}
+
+function applyChoice(num) {
+	switch(num) {
+		case 0:
+			$("#p1c1_choice").prop('disabled', false);
+			$("#p1c1_type").prop('disabled', false);
+			$("#p1c1_sel").prop('disabled', false);
+			$("#p1c1_apply").prop('disabled', false);
+	
+			addOptions("#p1c1_choice", p1Sel);
+			updateType("#p1c1");
+			break;
+		case 1:
+			$("[id^=p1c1_]").prop('disabled', true);
+			p1Sel = remove(p1Sel, processChoice("#p1c1"));
+
+			$("#p2c1_choice").prop('disabled', false);
+			$("#p2c1_type").prop('disabled', false);
+			$("#p2c1_sel").prop('disabled', false);
+			$("#p2c1_apply").prop('disabled', false);
+	
+			addOptions("#p2c1_choice", p2Sel);
+			updateType("#p2c1");
+			break;
+		case 2:
+			$("[id^=p2c1_]").prop('disabled', true);
+			p2Sel = remove(p2Sel, processChoice("#p2c1"));
+
+			$("#p2c2_choice").prop('disabled', false);
+			$("#p2c2_type").prop('disabled', false);
+			$("#p2c2_sel").prop('disabled', false);
+			$("#p2c2_apply").prop('disabled', false);
+	
+			addOptions("#p2c2_choice", p2Sel);
+			updateType("#p2c2");
+			break;
+		case 3:
+			$("[id^=p2c2_]").prop('disabled', true);
+			p2Sel = remove(p2Sel, processChoice("#p2c2"));
+
+			$("#p1c2_choice").prop('disabled', false);
+			$("#p1c2_type").prop('disabled', false);
+			$("#p1c2_sel").prop('disabled', false);
+			$("#p1c2_apply").prop('disabled', false);
+	
+			addOptions("#p1c2_choice", p1Sel);
+			updateType("#p1c2");
+			break;
+		case 4:
+			$("[id^=p1c2_]").prop('disabled', true);
+			p1Sel = remove(p1Sel, processChoice("#p1c2"));
+			
+			
+			$("#goal").html(generateGoal());
+			break;
+		default:
+			alert("Someone broke something... I'm gonna telllllll...");
+			return;
+	}
+}
+
+function generateGoal() {
+	var goal = ".setgoal Duel [ ";
+	var require = ""
+	var ban = ""
+	$.each(pids, function(key, value) {
+		if ($(value + "_choice option:selected").text() == 'Require') {
+			if (require != "")
+				require += ' and ';
+			if ($(value + "_num option:selected").text() != "")
+				require += $(value + "_num option:selected").text() + ' ';
+			require += $(value + "_sel option:selected").text();
+		}
+		else {
+			if (ban != "")
+				ban += ' and ';
+			ban += $(value + "_sel option:selected").text();
+		}
+	});
+	goal += "Require " + require + ", ";
+	goal += "Ban " + ban;
+	goal += " ]";
+	return goal;
+}
+
+function processChoice(id) {
+	if ($(id + "_choice option:selected").text() == "Ban") {
+		applyBan($(id + "_sel option:selected").text());
+	}
+	else if ($(id + "_choice option:selected").text() == "Require") {
+		if ($(id + "_type option:selected").text() == "Power Up") {
+			applyPReq($(id + "_sel option:selected").text(), parseInt($(id + "_num option:selected").text()));
+		}
+		else {
+			applyReq($(id + "_sel option:selected").text());
+		}
+	}
+	return $(id + "_choice option:selected").text();
+}
+
+function updateType(id) {
+	if ($(id + "_choice option:selected").text() == "Ban") {
+		var types = [];
+		if (iBan.length > 0)
+			types.push("Item")
+		if (pBan.length > 0)
+			types.push("Power Up")
+		addOptions(id + "_type", types);
+		updateSelect(id);
+	}
+	else if ($(id + "_choice option:selected").text() == "Require") {
+		var types = [];
+		if (iReq.length > 0)
+			types.push("Item")
+		if (pReq.length > 0)
+			types.push("Power Up")
+		if (boss.length == 4)
+			types.push("Last Boss")
+		if (!isRequired(lib['sporespawn']) && !isRequired(lib['crocomire']) && !isRequired(lib['botwoon']))
+			types.push("First Mini-Boss")
+		addOptions(id + "_type", types);
+		updateSelect(id);
+	}
+}
+
+function updateSelect(id) {
+	choice = $(id + "_choice option:selected").text();
+	$(id + "_num").prop('disabled', true).empty();
+
+	if ($(id + "_type option:selected").text() == "Item") {
+		if (choice == "Ban")
+			addOptions(id + "_sel", iBan);
+		else if (choice == "Require")
+			addOptions(id + "_sel", iReq);
+	}
+	else if ($(id + "_type option:selected").text() == "Power Up") {
+		if (choice == "Ban")
+			addOptions(id + "_sel", pBan);
+		else if (choice == "Require") {
+			addOptions(id + "_sel", pReq);
+			updateNum(id);
+		}
+	}
+	else if ($(id + "_type option:selected").text() == "Last Boss") {
+		addOptions(id + "_sel", boss);
+	}
+	else if ($(id + "_type option:selected").text() == "First Mini-Boss") {
+		addOptions(id + "_sel", mboss);
+	}
+}
+
+function updateNum(id) {
+	if ($(id + "_choice option:selected").text() == "Require") {
+		if($(id + "_type option:selected").text() == "Power Up") {
+			$(id + "_num").prop('disabled', false);
+			var values = []
+			switch($(id + "_sel option:selected").text()) {
+				case lib['missiles']:
+					for (i = 10; i <= maxM; i += 5)
+						values.push(i);
+					break;
+				case lib['supers']:
+					for (i = 10; i <= maxS; i += 5)
+						values.push(i);
+					break;
+				case lib['pbs']:
+					for (i = 10; i <= maxP; i += 5)
+						values.push(i);
+					break;
+				case lib['ets']:
+					for (i = 4; i <= maxE; i += 1)
+						values.push(i);
+					break;
+				case lib['reserve']:
+					for (i = 1; i <= maxR; i += 1)
+						values.push(i);
+					break;
+			}
+			addOptions(id + "_num", values);
+		}
+	}
+}
+
+function addOptions(id, options) {
+	$(id)
+		.empty()
+	$.each(options, function(key, value) {
+		$(id)
+			.append($("<option></option>")
+			.attr("value",value)
+			.text(value));
+		});
+}
+
+$(document).ready(function() {
+	$("#ver").html("(v" + [ver_major, ver_minor, ver_revision].join('.') + ")");
+	$("#p1name").on('input', function() {
+		$("#p1a").html( $(this).val() );
+		$("#p1b").html( $(this).val() );
+	});
+	$("#p2name").on('input', function() {
+		$("#p2a").html( $(this).val() );
+		$("#p2b").html( $(this).val() );
+	});
+	$("#p1c1_choice").change(function() {updateType("#p1c1");});
+	$("#p1c1_type").change(function() {updateSelect("#p1c1");});
+	$("#p2c1_choice").change(function() {updateType("#p2c1");});
+	$("#p2c1_type").change(function() {updateSelect("#p2c1");});
+	$("#p2c2_choice").change(function() {updateType("#p2c2");});
+	$("#p2c2_type").change(function() {updateSelect("#p2c2");});
+	$("#p1c2_choice").change(function() {updateType("#p1c2");});
+	$("#p1c2_type").change(function() {updateSelect("#p1c2");});
+
+	$("#p1c1_sel").change(function() {updateNum("#p1c1");});
+	$("#p2c1_sel").change(function() {updateNum("#p2c1");});
+	$("#p2c2_sel").change(function() {updateNum("#p2c2");});
+	$("#p1c2_sel").change(function() {updateNum("#p1c2");});
+	
+	$("#p1c1_apply").click(function() {applyChoice(1);});
+	$("#p2c1_apply").click(function() {applyChoice(2);});
+	$("#p2c2_apply").click(function() {applyChoice(3);});
+	$("#p1c2_apply").click(function() {applyChoice(4);});
+	
+	$("#resettool").click(function() {initTool();});
+	initTool();
+});
+
---
+++ leaderboards/index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Metroid Leaderboards</title>
+
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+
+<link rel="stylesheet" type="text/css" href="./css/main.css" />
+<link rel="stylesheet" type="text/css" href="./css/jquery.dataTables.css" />
+<link rel="icon" type="image/png" href="./images/metroid.ico" />
+
+<script type="text/JavaScript" src="./js/jquery.js"></script>
+<script type="text/JavaScript" language="javascript" src="./js/jquery.dataTables.min.js"></script>
+<script type="text/JavaScript" language="javascript" class="init">
+
+var images = ['landing.jpg',
+			  'metroid_ii_final_boss.jpg',
+			  'Mpt_bg.jpg'];
+
+$(document).ready(function() {
+	$('body').css('background-image', 'url(./images/' + images[Math.floor(Math.random() * images.length)] + ')');
+	$('#example').DataTable( {
+		"paging":	false,
+		"ordering":	false,
+		"info":		false
+	} )
+} );
+
+</script>
+</head>
+<body>
+<div id="curtain">&nbsp;</div>
+<div id="container">
+	<div id="header">
+		Under construction - Header goes here
+	</div>
+	<table id="example" class="display" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>Rank</th>
+				<th>Name</th>
+				<th>Time(IGT)</th>
+				<th>Time(RTA)</th>
+				<th>Video</th>
+				<th>Stream</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th>Rank</th>
+				<th>Name</th>
+				<th>Time(IGT)</th>
+				<th>Time(RTA)</th>
+				<th>Video</th>
+				<th>Stream</th>
+			</tr>
+		</tfoot>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>samthedigital</td>
+				<td>26:58</td>
+				<td>39:23</td>
+				<td>https://www.youtube.com/watch?v=Pw7aUzqkbfc</td>
+				<td>twitch.tv/samthedigital</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>dragonfangs</td>
+				<td>27:13</td>
+				<td>EMU</td>
+				<td>http://www.twitch.tv/dragonfangs/c/5308744</td>
+				<td>twitch.tv/dragonfangs</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>spidey-widey</td>
+				<td>27:24</td>
+				<td>40:20</td>
+				<td>http://www.youtube.com/watch?v=ut2gvcSiNQ4</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>eagle</td>
+				<td>27:31</td>
+				<td>39:55</td>
+				<td>https://www.youtube.com/watch?v=eaUNkR525vw</td>
+				<td>twitch.tv/makvaerk</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>kirbymastah</td>
+				<td>27:55</td>
+				<td>40:28</td>
+				<td>https://www.youtube.com/watch?v=djbd8QKxKhA</td>
+				<td>twitch.tv/kirbymastah</td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>mobiusman</td>
+				<td>28:12</td>
+				<td>40:39</td>
+				<td>https://www.youtube.com/watch?v=SYBvAla1-Uk</td>
+				<td>twitch.tv/mobiusman</td>
+			</tr>
+			<tr>
+				<td>7</td>
+				<td>thomaz</td>
+				<td>28:20</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>solasel</td>
+				<td>28:22</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>9</td>
+				<td>loicleking</td>
+				<td>28:32</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>10</td>
+				<td>zx497</td>
+				<td>28:33</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/zx497</td>
+			</tr>
+			<tr>
+				<td>11</td>
+				<td>secretkey</td>
+				<td>28:57</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/secretkey90</td>
+			</tr>
+			<tr>
+				<td>12</td>
+				<td>peril1337</td>
+				<td>29:30</td>
+				<td>EMU</td>
+				<td>http://www.twitch.tv/peril1337/c/5033592</td>
+				<td>twitch.tv/peril1337</td>
+			</tr>
+			<tr>
+				<td>13</td>
+				<td>jaggerg</td>
+				<td>29:45</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/jaggerg</td>
+			</tr>
+			<tr>
+				<td>14</td>
+				<td>transience</td>
+				<td>29:58</td>
+				<td></td>
+				<td>https://archive.org/details/MetroidZM_SS_2958</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>15</td>
+				<td>akvao</td>
+				<td>30:05</td>
+				<td>42:49</td>
+				<td>http://www.twitch.tv/akvao/c/4826765</td>
+				<td>twitch.tv/akvao</td>
+			</tr>
+			<tr>
+				<td>16</td>
+				<td>haaria</td>
+				<td>30:38</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/haaria</td>
+			</tr>
+			<tr>
+				<td>17</td>
+				<td>darko</td>
+				<td>30:48</td>
+				<td>EMU</td>
+				<td>http://www.twitch.tv/darkomegamagic/c/3682451</td>
+				<td>twitch.tv/darkomegamagic</td>
+			</tr>
+			<tr>
+				<td>18</td>
+				<td>pyrq</td>
+				<td>31:04</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/pyrq</td>
+			</tr>
+			<tr>
+				<td>19</td>
+				<td>jackal_boy</td>
+				<td>31:47</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>20</td>
+				<td>axodo</td>
+				<td>31:53</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>21</td>
+				<td>master-88</td>
+				<td>32:23</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>22</td>
+				<td>imoutoframen</td>
+				<td>33:17</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/imoutoframen</td>
+			</tr>
+			<tr>
+				<td>23</td>
+				<td>mickeygnou</td>
+				<td>33:29</td>
+				<td></td>
+				<td></td>
+				<td>twitch.tv/mickeygnou</td>
+			</tr>
+			<tr>
+				<td>24</td>
+				<td>emlun</td>
+				<td>33:34</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>25</td>
+				<td>c0rrupt3d</td>
+				<td>33:59</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>26</td>
+				<td>elb0ludo</td>
+				<td>34:03</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>27</td>
+				<td>lerd praten</td>
+				<td>34:12</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>28</td>
+				<td>shadowmario3</td>
+				<td>35:14</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>29</td>
+				<td>stupiddumb</td>
+				<td>35:35</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>30</td>
+				<td>ing-x</td>
+				<td>35:38</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+			
+</body>
+</html>
